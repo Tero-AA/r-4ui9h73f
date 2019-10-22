51a2399b6bc85f87680f7d0775852781be456b64
Andrés Ayala <53452577+Tero-AA@users.noreply.github.com>
2019-10-22 11:41:28 -0500

    Add the text repeater

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.updateText = this.updateText.bind(this)
+
+    this.state = {
+      text: ""
+    }
+  }
+
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.updateText} />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
+  }
+
+  updateText(e) {
+    this.setState({
+      text: e.target.value
+    })
   }
 }
 
